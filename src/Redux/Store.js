a9ce708ace1bf00420354bca4590778866ad4aaf
Abd0Khalifa <abdoali345757@gmail.com>
2025-01-27 00:33:15 +0200

    remove redux devtool

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,12 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension'
 import favReducer from "./Reducers";
 import { thunk } from "redux-thunk";
 
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(favReducer,composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(favReducer,applyMiddleware(thunk));
 // const store = createStore(favReducer, composeEnhancers());
 
 export default store;
